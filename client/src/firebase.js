16cabda41de8d9ecb063984f6217ec676ee88f05
Tokyo-al <stelamucodemi@gmail.com>
2025-06-15 15:40:14 +0300

    complete image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,9 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyA5Iy7Icemj7-0HS2uvS4QnDIwGuabQiCU",
   authDomain: "mern-estate-50001.firebaseapp.com",
   projectId: "mern-estate-50001",
   storageBucket: "mern-estate-50001.appspot.com",
@@ -9,4 +11,8 @@ const firebaseConfig = {
   appId: "1:904272878580:web:21c8188682a065da6177a1"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export { app };
